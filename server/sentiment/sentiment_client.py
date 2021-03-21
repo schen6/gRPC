6fa8a664d1289ccfe338d3977c9efadbee74d5f6
Little-Frog-233 <1342468180@qq.com>
2021-03-21 08:23:16 +0800

    update

--- server/sentiment/sentiment_client.py
+++ server/sentiment/sentiment_client.py
@@ -14,7 +14,7 @@ def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('47.96.179.14:50051') as channel:
+    with grpc.insecure_channel('127.0.0.1:50051') as channel:
         stub = sentiment_pb2_grpc.SentimentStub(channel)
         response = stub.GetSentiment(sentiment_pb2.TextInput(text='你妈死了'))
         print("Sentiment client received: " + response.result)
